30f5b537d83945a1ea380fbe9f5c1fc097a8d7af
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-01 17:49:47 -0400

    [jfunctions] boilerplates for the files

---
+++ scripts/map-jnumber.js
@@ -0,0 +1,16 @@
+/**
+ * /scripts/map-jnumber.js
+ * Performs functions on a J-Number.
+ *
+ * By        : Leomar Duran <https://github.com/lduran2>
+ * When      : 
+ * Where     : Community College of Philadelphia
+ * For       : CIS 114/JavaScript I
+ * Version   : 
+ * Canonical : 
+ *
+ * CHANGELOG :
+ *     v - 
+ *         
+ */
+'use strict';

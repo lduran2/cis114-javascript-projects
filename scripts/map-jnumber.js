abb2943131bac71510db433871fd79b6812a3e63
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-01 21:58:23 -0400

    implemented the unique ordered digit functions

--- scripts/map-jnumber.js
+++ scripts/map-jnumber.js
@@ -3,13 +3,16 @@
  * Performs functions on a J-Number.
  *
  * By        : Leomar Duran <https://github.com/lduran2>
- * When      : 2021-11-01t21:22
+ * When      : 2021-11-01t21:57
  * Where     : Community College of Philadelphia
  * For       : CIS 114/JavaScript I
- * Version   : 1.1.1
+ * Version   : 1.2.0
  * Canonical : https://github.com/lduran2/cis114-javascript-projects/blob/master/scripts/map-jnumber.js
  *
  * CHANGELOG :
+ *     v1.2.0 - 2021-11-01t21:57
+ *         implemented the unique ordered digit functions
+ *
  *     v1.1.1 - 2021-11-01t21:22
  *         bit mask for unique digits
  *
@@ -22,17 +25,89 @@
 'use strict';
 
 function main() {
-	const JNUMBER = '00224250';	/* my J-Number used for testing */
-	const JSUM = sumDigitsIn(JNUMBER);	/* sum of digits of the J-Number */
-	const UNIQUE_DIGITS = uniqueOrderedDigits(JNUMBER);	/* find unique digits */
+	/* get the user provided value */
+	const USER_PROVIDED_VALUE = getUserInput();
 
-	/* print the JSUM and unique digits */
-	console.log(JSUM);
-	console.log(UNIQUE_DIGITS);
+	/* perform the operations */
+	const RESULT1 = f0(USER_PROVIDED_VALUE);
+	const RESULT2 = f0(RESULT1);
+	const RESULT3 = f1(RESULT2);
+	const RESULT4 = f2(RESULT3);
+	const RESULT5 = f3(RESULT4);
+	const RESULT6 = f2(RESULT5);
+	const RESULT7 = f1(RESULT6);
+	const RESULT8 = f3(RESULT7);
+
+	/* print the result */
+	console.log(`You provided the number ${USER_PROVIDED_VALUE}. Your result is ${RESULT8}.`);
 
 	/* finished */
 	console.log('Done.');
-} /* function main() */
+} /* end function main() */
+
+/* the length of UNIQUE_DIGITS is 4 for the given JNUMBER,
+ * so make f[0..3] functions */
+
+/**
+ * function for first unique digit
+ * @param input : Number = the function input
+ * @return the mapping from the given input
+ */
+function f0(input) {
+	return f(0, input);
+} /* end function f0(input) */
+
+/**
+ * function for second unique digit
+ * @param input : Number = the function input
+ * @return the mapping from the given input
+ */
+function f1(input) {
+	return f(1, input);
+} /* end function f1(input) */
+
+/**
+ * function for third unique digit
+ * @param input : Number = the function input
+ * @return the mapping from the given input
+ */
+function f2(input) {
+	return f(2, input);
+} /* end function f2(input) */
+
+/**
+ * function for fourth unique digit
+ * @param input : Number = the function input
+ * @return the mapping from the given input
+ */
+function f3(input) {
+	return f(3, input);
+} /* end function f3(input) */
+
+/**
+ * Function for any index of the unique digits and a given input.
+ * @param index : Number = of the unique ordered digits to decide the
+ *     function
+ * @param input : Number = the function input
+ * @return the mapping from the given input
+ */
+function f(index, input) {
+	let result;
+	/* if ith unique digit is zero or even (oddness bit is clear) */
+	if (!(UNIQUE_DIGITS[index] & 1)) {
+		/* add with input and JSUM */
+		result = (input + UNIQUE_DIGITS[index] + JSUM);
+	} /* end if (!(UNIQUE_DIGITS[index] & 1)) */
+	else {
+		/* otherwise, multiply with input and JSUM */
+		result = (input * UNIQUE_DIGITS[index] * JSUM);
+	} /* end (!(UNIQUE_DIGITS[index] & 1)) else */
+	return result;
+} /* end function f(index, input) */
+
+function getUserInput() {
+	return 12312;
+}
 
 /**
  * Reduces a number parsed from a string by adding all digits.
@@ -47,8 +122,13 @@ function sumDigitsIn(aString) {
 		sum += Number.parseInt(CHAR);
 	} /* for (const CHAR of aString.split('')) */
 	return sum;
-} /* function sumDigitsIn(aString) */
+} /* end function sumDigitsIn(aString) */
 
+/**
+ * Find the unique digits in aString by their original order.
+ * @param aString : String = to reduce
+ * @return the unique digits in order
+ */
 function uniqueOrderedDigits(aString) {
 	const UNIQUE = [];	/* the digits so far */
 	let found = 0;	/* the digits found so far, as a bit mask */
@@ -62,9 +142,18 @@ function uniqueOrderedDigits(aString) {
 			UNIQUE.push(DIGIT);
 			/* mark as found */
 			found |= mask;
-		} /* if (!(found & mask)) */
-	} /* for (const CHAR of aString.split('')) */
+		} /* end if (!(found & mask)) */
+	} /* end for (const CHAR of aString.split('')) */
 	return UNIQUE;
-} /* function uniqueOrderedDigits(aString) */
+} /* end function uniqueOrderedDigits(aString) */
+
+/** my J-Number used for testing */
+const JNUMBER = '00224250';
+
+/** sum of digits of the J-Number */
+const JSUM = sumDigitsIn(JNUMBER);
+
+/** find unique digits */
+const UNIQUE_DIGITS = uniqueOrderedDigits(JNUMBER);
 
 main();
